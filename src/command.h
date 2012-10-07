f7217e466b33061b623d7279504f5ae467bf981d
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-10-07 20:15:54 +0200

    src/command.h: Added a doxygen file header.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- src/command.h
+++ src/command.h
@@ -1,3 +1,9 @@
+/*!
+ * \file src/command.h
+ *
+ * \brief Prototypes for command execution.
+ */
+
 /*
  *                            COPYRIGHT
  *
@@ -22,9 +28,6 @@
  *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
  *  Thomas.Nau@rz.uni-ulm.de
  *
- */
-
-/* prototypes for command execution
  */
 
 #ifndef	PCB_COMMAND_H
