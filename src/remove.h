cff66648644367643903b024d9ec17ef1a1699fa
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-10-26 17:47:16 +0200

    src/remove.h: Added a doxygen file header.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- src/remove.h
+++ src/remove.h
@@ -1,3 +1,9 @@
+/*!
+ * \file src/remove.h
+ *
+ * \brief Prototypes for remove routines.
+ */
+
 /*
  *                            COPYRIGHT
  *
@@ -22,9 +28,6 @@
  *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
  *  Thomas.Nau@rz.uni-ulm.de
  *
- */
-
-/* prototypes for remove routines
  */
 
 #ifndef	PCB_REMOVE_H
